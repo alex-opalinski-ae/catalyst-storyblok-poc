34e20d5ad398f02248a18acfc577cffe8db47666
Alex Opalinski <alex.opalinski@americaneagle.com>
2025-10-17 13:56:31 -0500

    storyblok base url

--- core/lib/content-security-policy.ts
+++ core/lib/content-security-policy.ts
@@ -2,7 +2,7 @@ import builder from 'content-security-policy-builder';
 
 const makeswiftEnabled = !!process.env.MAKESWIFT_SITE_API_KEY;
 
-const makeswiftBaseUrl = process.env.MAKESWIFT_BASE_URL || 'https://app.makeswift.com';
+const makeswiftBaseUrl = process.env.MAKESWIFT_BASE_URL || 'https://app.storyblok.com';
 
 const frameAncestors = makeswiftEnabled ? makeswiftBaseUrl : 'none';
 
